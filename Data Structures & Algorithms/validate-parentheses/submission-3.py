3e04352726beb7732ea4cc31b65e8150de6bd26d
Anton Berisha <Anton.Berisha01@gmail.com>
2026-04-09 05:25:35 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        parentheses_map = { "]":"[", ")":"(", "}":"{" }
+        stack = []
+
+        for c in s:
+            if c in parentheses_map:
+                if len(stack) < 1: return False
+                ele = stack.pop()
+                if  ele != parentheses_map[c]:
+                    return False            
+            else:
+                stack.append(c)
+        return len(stack) == 0
